74869370efdf35aa267076c65346f7ae29ab8083
EmirhanYavuz-git <yavuzemirhan222@gmail.com>
2023-08-23 18:03:07 +0300

    Added pojo into spring container

---
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -0,0 +1,22 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class ComputerTest {
+
+    public static void main(String[] args) {
+
+
+        ApplicationContext container = new AnnotationConfigApplicationContext();
+
+
+
+
+
+
+
+
+    }
+}
